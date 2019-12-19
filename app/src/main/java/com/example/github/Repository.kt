d76e4b0853f63842b92c05ec2f9111a9b84f8a96
Peggy <pehgy5342z@gmail.com>
2019-12-19 17:43:01 +0800

    UI design

---
+++ app/src/main/java/com/example/github/Repository.kt
@@ -0,0 +1,12 @@
+package com.example.github
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Repository : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_repository)
+    }
+}
